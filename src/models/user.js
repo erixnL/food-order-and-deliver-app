a07b460b19be1e58afcceb8b222e55a53bc8e1b2
erixnL <96608889+erixnL@users.noreply.github.com>
2024-03-30 20:29:03 +1100

    add user data schema

---
+++ src/models/user.js
@@ -0,0 +1,17 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema ({
+    email: {
+        type: String,
+        unique: [true, 'Email already exists!'],
+        required: [true, 'Email is required!'],
+      },
+      username: {
+        type: String,
+        required: [true, 'Username is required!']
+      }
+})
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
