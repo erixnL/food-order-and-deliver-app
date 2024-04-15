9e1917b2bcbae067f59ef5f8a8d7ed552b53d932
erixnL <96608889+erixnL@users.noreply.github.com>
2024-04-15 10:17:23 +1000

    update database path

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcrypt';
-import { connectToDB } from '@/utils/database';
+import { connectToDB } from '@/utlis/database';
 import User from '@/models/user';
 
 const handler = NextAuth({
